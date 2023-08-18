9fad2ca79b1500ea4c7ad4719fb951f33e212ed7
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-18 11:25:09 +0100

    landing page setup

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,27 @@
+import React from 'react'
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
 const Landing = () => {
-    return (
-        <h1>landing page</h1>
-    )
+    return <main>
+        <nav>
+            <img src={logo} alt='logo'  className='logo'/>
+        </nav>
+        <div className='container page'>
+            <div className='info'>
+                <h1>job <span>tracking</span> app</h1>
+                <p>
+                    I'm baby small batch unicorn post-ironic prism gorpcore trust fund live-edge ramps stumptown 3 wolf moon ethical bicycle
+                    rights crucifix mixtape. Single-origin coffee plaid kogi bodega boys YOLO retro. Tonx meditation roof party, hella irony
+                    shabby chic selfies kogi schlitz microdosing chicharrones retro meggings. DSA iceland hoodie polaroid street art, af meggings
+                    marfa bitters kombucha +1 kinfolk chicharrones JOMO hexagon.
+                    Shabby chic whatever iceland, next level cronut plaid normcore godard synth dreamcatcher irony readymade gentrify hell of.
+                    Grailed enamel pin selvage cold-pressed.
+                </p>
+                <button className='btn btn-hero'>Login/Register</button>
+            </div>
+            <img src={main} alt='main' className='img main-img'/>
+        </div>
+    </main>  
 }
 export default Landing
