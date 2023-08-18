663b668bc75b209a597af6e0222b58b4d207b43d
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-18 11:12:29 +0100

    landing page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          jobster
-        </h1>
-      </header>
+    <div >
+      <Landing />
     </div>
   );
 }
---
+++ src/pages/Landing.js
@@ -0,0 +1,9 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+    return (
+        <h1>landing page</h1>
+    )
+}
+export default Landing
