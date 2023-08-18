d19a2c94e74fe6886fdd1b8f19f69f8ed7f6317a
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-18 10:57:23 +0100

    normalize css installation and setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        jobster
+        <h1>
+          jobster
+        </h1>
       </header>
     </div>
   );
