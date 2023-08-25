f04161f17a44d7ddce10cda4f85e91867f3ed1c0
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-25 11:58:33 +0100

    add user details to local storage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,11 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
 import customFetch from '../../utils/axios';
+import { addUserToLocalStorage, getUserFromLocalStorage } from '../../utils/localStorage';
 
 
 const initialState = {
     isLoading: false,
-    user: null
+    user: getUserFromLocalStorage()
 }
 
 export const registerUser = createAsyncThunk(
@@ -42,6 +43,7 @@ const userSlice = createSlice({
                 const { user } = payload
                 state.loading = false
                 state.user = user
+                addUserToLocalStorage(user)
                 toast.success(`Hello dear ${user.name}`)
             })
             .addCase(registerUser.rejected, (state , { payload }) => {
@@ -55,6 +57,7 @@ const userSlice = createSlice({
                 const { user } = payload
                 state.loading = false
                 state.user = user
+                addUserToLocalStorage(user)
                 toast.success(`Welcome back ${user.name}`)
             })
             .addCase(loginUser.rejected, (state , { payload }) => {
