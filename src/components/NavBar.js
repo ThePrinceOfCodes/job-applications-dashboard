4863960b78cdbcdf076a9c4c35206da7b412af80
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-25 13:30:00 +0100

    navbar setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,37 @@
 import React from "react";
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome, FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+import Logo from "./Logo";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const NavBar = () => {
-    return <Wrapper>nav bar</Wrapper>
+    const { user } = useSelector((store) => store.user)
+    const dispatch = useDispatch()
+
+    return <Wrapper>
+        <div className="nav-center">
+            <button type='button' className="toggle-btn" onClick={console.log('toggle side abr')}>
+                <FaAlignLeft/>
+            </button>
+            <div>
+                <Logo />
+                <h3 className="logo text">dashboard</h3>
+            </div>
+            <div className="btn-container">
+                <button type="button" className="btn" onClick={console.log('clicked')}>
+                    <FaUserCircle />
+                    {user? user.name: null}
+                    <FaCaretDown />
+                </button>
+                <div className="dropdown show-dropdown">
+                    <button type="button" className="dropdown-btn" onClick={console.log('logout user')}>
+                        logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    </Wrapper>
 }
 
 export default NavBar
