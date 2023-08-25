e11c7e94abda00d105476276f7eb103d0c346e63
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-25 01:27:49 +0100

    login and registration page setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,15 +22,29 @@ const Register = () => {
         console.log(e.target);
     }
 
+    const toggleMember = () => {
+        setValues({...values, isMember: !values.isMember})
+    }
+
     return <Wrapper className="full-page">
         <form className="form" onSubmit={onSubmit}>
             <Logo />
-            <h3>Login</h3>
+            <h3> { values.isMember ? 'Login' : 'Register'}</h3>
+            {!values.isMember && (
+                <FormRow name='name' type='name' value={values.name} handleChange={handleChange} labelText='name' />
+            )}
             <FormRow name='email' type='email' value={values.email} handleChange={handleChange} labelText='email' />
             <FormRow name='password' type='password' value={values.password} handleChange={handleChange} labelText='password' /> 
             <button type="submit" className="btn btn-block">
-                submit
+                { values.isMember ? 'Login' : 'Register'}
             </button>
+            <p>
+                {values.isMember ? ('Not a member?') : 'Already a member'}
+                 <button type="button" className="member-btn" onClick={toggleMember}>
+                { !values.isMember ? 'Login' : 'Register'}
+            </button>
+            </p>
+
        </form>
     </Wrapper>
 }
