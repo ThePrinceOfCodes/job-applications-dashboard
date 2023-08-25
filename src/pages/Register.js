105e35e7684ebc16941b06c9899b4fffc7aa190c
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-25 01:54:10 +0100

    react toastify setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useState, useEffect} from 'react'
 import { Logo,FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { toast } from "react-toastify"
 
 const initialState = {
     name: '',
@@ -23,6 +24,7 @@ const Register = () => {
         e.preventDefault()
         const { name, email, password, isMember } = values
         if (!email || !password || (!isMember && !name)) {
+            toast.error('please fill out all fields')
             return
         }
 
