cc3f2c51c1a186fa647fc1fe11b5dcf04c3a8cbb
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-26 02:09:42 +0100

    wip

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
 const Profile = () => {
   const { isLoading, user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
 const [userData,setUserData] = useState({
-  name:user?.name ||'',
-  email:user?.email ||'',
-  lastName:user?.lastName ||'',
-  location:user?.location ||''
+  name:user.name || '',
+  email:user.email || '',
+  lastName:user.lastName || '',
+  location:user.location || ''
 })
 
   const handleSubmit = (e) => {
