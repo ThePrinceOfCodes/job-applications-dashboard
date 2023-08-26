20e398dd8a359c58a3579f090eca97220bddc6c0
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-26 01:45:58 +0100

    small side bar navlink setup

--- src/components/SmallSideBar.js
+++ src/components/SmallSideBar.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Wrapper from '../assets/wrappers/SmallSidebar'
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
 import Logo from './Logo'
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
-import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 
 const SmallSideBar = () => {
@@ -13,7 +12,7 @@ const SmallSideBar = () => {
     const dispatch = useDispatch()
 
     const toggle = () => {
-        dispatch(toggleSidebar())
+        dispatch(toggleSidebar)
     }
 
     return <Wrapper>
@@ -25,25 +24,7 @@ const SmallSideBar = () => {
                 <header>
                     <Logo />
                 </header>
-                <div className="nav-links">
-                    {links.map((link) => {
-                    const { text, path, id, icon } = link;
-
-                    return (
-                        <NavLink
-                            to={path}
-                            className={({ isActive }) =>
-                                isActive ? 'nav-link active' : 'nav-link'
-                            }
-                            key={id}
-                            onClick={()=>toggle()}
-                        >
-                            <span className='icon'>{icon}</span>
-                            {text}
-                        </NavLink>
-                    );
-                    })}
-                </div>
+                <NavLinks toggle={toggle}/>
             </div>
         </div>
     </Wrapper>
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import FormRow from "./FormRow";
 import NavBar from "./NavBar";
 import BigSideBar from "./BigSideBar";
 import SmallSideBar from "./SmallSideBar";
+import NavLinks from "./NavLinks";
 
-export { Logo, FormRow, BigSideBar, SmallSideBar, NavBar }
+export { Logo, FormRow, BigSideBar, SmallSideBar, NavBar, NavLinks }
