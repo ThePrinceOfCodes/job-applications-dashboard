c6cb9d9ff221f388a355bdd5d361e1614bd46d0b
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-28 15:44:06 +0100

    dispatch job edit function when condition is met

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -3,7 +3,7 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange, clearValues, createJob} from '../../features/job/jobSlice';
+import { handleChange, clearValues, createJob, editJob} from '../../features/job/jobSlice';
 
 const AddJob = () => {
     const { user } = useSelector((store)=>store.user)
@@ -29,9 +29,12 @@ const AddJob = () => {
         return;
         }
 
-        dispatch(createJob({ position, company, jobLocation, jobType, status }))
+        if (isEditing) {
+            dispatch(editJob({ jobId: editJobId, job: { position, company, jobLocation, jobType, status } }))
+            return
+        }
         
-        
+        dispatch(createJob({ position, company, jobLocation, jobType, status })) 
     };
 
     const handleJobInput = (e) => {
