551873c6245f87388eec214d419e266ff022d30b
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-29 00:30:12 +0100

    stat item component ui

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/StatItem";
 
-const StatItem = () => {
-    return <h2>
-    stat item
-    </h2>
+const StatItem = ({count, title, icon, color, bcg}) => {
+    return <Wrapper color={color} bcg={bcg}>
+        <header>
+            <span className="count">{count}</span>
+            <span className="icon">{icon}</span>
+        </header>
+        <h5 className="title">{title}</h5>
+    </Wrapper>
 }
 
 export default StatItem
