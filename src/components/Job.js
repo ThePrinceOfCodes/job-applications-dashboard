1d73742a6df05cb3f29f3f161cfa7fa17e873b24
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-28 14:25:49 +0100

    job component ui setup

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Job";
+import { useDispatch } from "react-redux";
 
-const Job = () => {
+const Job = ({ _id:id, position, company, jobLocation, jobType, createdAt, status }) => {
+    const dispatch = useDispatch()
 
-    return <div>
-        <h1> job</h1>
-    </div>
+    return <Wrapper>
+        <header>
+            <div className="main-icon">
+                {company.charAt(0)}
+            </div>
+            <div className="info">
+                <h5>{position}</h5>
+                <p>{company}</p>
+            </div>
+        </header>
+        <div className="content">
+            <div className="content-center">
+                <h4>more content</h4>
+                <div className={`status ${status}`}>
+                    {status}
+                </div>
+            </div>
+            <footer>
+                <div className="actions">
+                    <Link to='/add-job' className="btn edit-btn" onClick={console.log(123321)}>Edit</Link>
+                    <button className="btn delete-btn" onClick={console.log('delete')}> delete</button>
+                </div>
+            </footer>
+        </div>
+    </Wrapper>
 }
 
 export default Job
