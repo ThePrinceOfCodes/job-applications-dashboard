4de2df99fd97d2c6d9043cdffebf07b66549851e
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-29 00:50:38 +0100

    area chart set up

--- src/components/AreaChart.js
+++ src/components/AreaChart.js
@@ -1,9 +1,25 @@
-import React from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
-const AreaChat = () => {
-    return <h1>
-        AreaChat
-    </h1>
-}
+const AreaChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#1e3a8a' fill='#3b82f6' />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default AreaChat
+export default AreaChartComponent;
