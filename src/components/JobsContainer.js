00fb5f6781e327ac02922958170c1e775858908f
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-28 13:30:02 +0100

    is loading component

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -3,15 +3,14 @@ import Job from "./Job";
 import { useEffect } from "react";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
+import Loading from "./Loading";
 
 const JobsContainer = () => {
     const { jobs, isLoading } = useSelector((store) => store.allJobs);
     const dispatch = useDispatch();
 
     if (isLoading) {
-        return <Wrapper>
-            <h2> Loading...</h2>
-        </Wrapper>
+        return <Loading center/>
     }
 
     if (jobs.length === 0) {
