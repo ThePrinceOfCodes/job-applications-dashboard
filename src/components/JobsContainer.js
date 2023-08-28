8bdcac6aba774b487c9ea2165a6db1970383df65
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-28 13:49:48 +0100

    get jobs reducer

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -4,10 +4,15 @@ import { useEffect } from "react";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
 import Loading from "./Loading";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 const JobsContainer = () => {
     const { jobs, isLoading } = useSelector((store) => store.allJobs);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllJobs())
+    },[])
 
     if (isLoading) {
         return <Loading center/>
