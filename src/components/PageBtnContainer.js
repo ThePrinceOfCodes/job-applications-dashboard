f9ee497b5fc31b620626105cf3d3858961ba413e
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-29 01:51:03 +0100

    bug fix

--- src/components/PageBtnContainer.js
+++ src/components/PageBtnContainer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 import Wrapper from '../assets/wrappers/PageBtnContainer';
 import { useSelector, useDispatch } from 'react-redux';
